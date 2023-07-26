bf0603f93b6cc1b5068fb6ecd566dea9ee23f436
ardaalkan <ardaaa.alk@gmail.com>
2023-07-26 16:53:57 +0300

    added 404 for post page

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -1,4 +1,15 @@
+import { DUMMY_POSTS } from "@/DUMMY_DATA";
+import { PageNotFoundError } from "next/dist/shared/lib/utils";
+import { notFound } from "next/navigation";
 import React from "react";
+
+export const generateStaticParams = async () => {
+  return DUMMY_POSTS.map((post) => {
+    return {
+      slug: post.slug,
+    };
+  });
+};
 
 const Page = ({
   params,
@@ -7,7 +18,13 @@ const Page = ({
     slug: string;
   };
 }) => {
-  return <div>{params.slug}</div>;
+  const post = DUMMY_POSTS.find((post) => post.slug === params.slug);
+
+  if (!post) {
+    notFound();
+  }
+
+  return <div>{post?.title}</div>;
 };
 
 export default Page;
